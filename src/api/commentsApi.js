61a2ff7bdfb6dc835d3b6479bc639c727521fbb4
Ваше Имя <you@example.com>
2023-03-27 17:24:53 +0400

    edited Loader, vars.scss, utils, etc

--- src/api/commentsApi.js
+++ src/api/commentsApi.js
@@ -1,5 +1,5 @@
 import instance from "./instance";
-import paramsToString from "./utils/paramsToString";
+import paramsToString from "../utils/paramsToString";
 
 export const requestComments = (params = {}) =>
   instance.get(`/comments?${paramsToString(params)}`);
--- src/store/comment/actions.js
+++ src/store/comment/actions.js
@@ -29,14 +29,12 @@ export const fetchComments = (params) => async (dispatch) => {
 
   try {
     const response = await requestComments(params);
-    setTimeout(() => {
       dispatch({
         type: types.FETCH_COMMENTS_SUCCESS,
         payload: {
           data: response.data,
         },
       });
-    }, 2000);
   } catch (error) {
     dispatch({
       type: types.FETCH_COMMENTS_FAILURE,
